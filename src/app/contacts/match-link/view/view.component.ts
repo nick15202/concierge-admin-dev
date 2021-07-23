8aa4940c5667313d7ba8d03397406f9d95aaae2d
Appwrk <Appwrk@DESKTOP-O7TFL8U>
2021-07-23 14:51:18 +0530

    pending + create landing page issue fixed

--- src/app/contacts/match-link/view/view.component.ts
+++ src/app/contacts/match-link/view/view.component.ts
@@ -1,21 +1,21 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+declare let $: any;
 @Component({
     selector: 'app-view-component',
     templateUrl: './view.component.html',
     styleUrls: ['./view.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ViewComponent implements OnInit {
+export class ViewComponent {
     public contacts: any;
     public succeededListings: any = [];
+    public data: any;
     public matchingStatus: any;
     public pendingListings: any = [];
     public failedListings: any = [];
     public matchLinkIdFromRoute: any;
-
     constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) {
         const headers = new HttpHeaders().set('x-token', 'seAu4yf9Dhj0DQWKNemssni9pxDYMP')
             .set('content-type', 'application/json');
@@ -26,16 +26,16 @@ export class ViewComponent implements OnInit {
             .get<any[]>(`https://ooba-digitaloffice.form.io/matchLink/submission/${this.matchLinkIdFromRoute}`, { headers })
             .subscribe((res: any) => {
                 if (res) {
+                    this.data = res;
                     this.contacts = res.data.contact.data;
-                    this.succeededListings = res.data.listings.filter(p => p.status === 'succeeded');
-                    this.pendingListings = res.data.listings.filter(p => p.status === 'pending');
-                    this.failedListings = res.data.listings.filter(p => p.status === 'failed');
+                    this.succeededListings = res.data.listings.filter(p => p.status.indexOf('succeeded') >= 0);
+                    this.pendingListings = res.data.listings.filter(p => p.status.indexOf('pending') >= 0);
+                    this.failedListings = res.data.listings.filter(p => p.status.indexOf('failure') >= 0);
                     this.matchingStatus = res.data.matchingStatus;
                 }
             });
     }
     getSchedule(value) {
-
         if (value) {
             if (value.indexOf('Date') < 0) {
                 return false;
@@ -62,8 +62,26 @@ export class ViewComponent implements OnInit {
             }
         }
     }
-    ngOnInit() {
 
+    createlandingPage() {
+        $('#landingBtn > i').addClass('fa-refresh  fa-spin');
+        setTimeout(() => {
+            const headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            };
+            fetch('https://hook.integromat.com/drttgj2phutrlccvtcrtuxfm93zahaeu', {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: 'POST',
+                body: JSON.stringify(this.data)
+            })
+                .then(function (res) {
+                });
+            $('#landingBtn > i').removeClass('fa-refresh  fa-spin');
+        }, 1000);
     }
 }
 
