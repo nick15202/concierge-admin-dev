10698e56034f17aa1719fa72cd75ecf174fef6be
expert-dev1 <odev687@gmail.com>
2021-08-17 12:40:18 +0530

    changes for submission form

--- src/app/contacts/match-link/edit/edit.component.ts
+++ src/app/contacts/match-link/edit/edit.component.ts
@@ -9,4 +9,19 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EditComponent extends FormioResourceEditComponent {
+
+    ngOnInit() {
+        for (let i = 0; i < this.service.resource.data.listings.length; i++) {
+            if (this.service.resource.data.listings[i].feedbackForm && this.service.resource.data.listings[i].feedbackForm._id) {
+                const feedbackForm = {
+                    '_id': this.service.resource.data.listings[i].feedbackForm._id
+                };
+                const scheduleForm = {
+                    '_id': this.service.resource.data.listings[i].scheduleForm._id
+                };
+                this.service.resource.data.listings[i].feedbackForm = feedbackForm;
+                this.service.resource.data.listings[i].scheduleForm = scheduleForm;
+            }
+        }
+    }
 }
